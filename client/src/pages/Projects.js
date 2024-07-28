4ef7dce0f67b7077fdae7b988c1fab0ea46366b6
Elrak01 <105533430+nicholas-a-karle@users.noreply.github.com>
2024-07-27 18:51:39 -0700

    image test

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -8,13 +8,13 @@ const Projects = () => {
     <div className='projectCardContainer'>
       <ProjectCard
         title="brewith.us"
-        image={`${process.env.PUBLIC_URL}/projectImages/brewithus.png`}
+        image={`/projectImages/brewithus.PNG`}
         description="This website is built to discover nearby coffee shops, tea shops, or anywhere that prepares and sells either using AI. I worked on the backend and external API calls and dataflow."
         link="https://www.brewith.us/"
       />
       <ProjectCard
         title="Coco's Courier Company"
-        image={`${process.env.PUBLIC_URL}/projectImages/cocos.png`}
+        image={`/projectImages/cocos.PNG`}
         description="This game is about girl who flies on a broomstick to deliver packages. It was built in Unity. I worked on what I would call the backend"
         link="https://yawgmoth.github.io/CocoBuild/"
       />
