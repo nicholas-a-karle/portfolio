80c7ba57212f6d43d0fac1ede8fac211c4d6175f
Elrak01 <105533430+nicholas-a-karle@users.noreply.github.com>
2024-08-02 18:54:01 -0700

    link on home page removed

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
     <h2>Nicholas Karle</h2>
     <p>Hi, I'm a recent computer science graduate aspiring to become a software engineer</p>
     <p>If you want to learn more about me, look at the <Link className="link" to="/about">about</Link> page. 
-      If you need to contact me, check the <Link className="link" to="/contact">contact</Link> page. 
+      If you need to contact me, check the bottom of the page. 
       If just want to check out my projects, check the <Link className="link" to="/projects">projects</Link> page.</p>
   </Card>
   );
